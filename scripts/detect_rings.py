0657d272531b25c00e032d0fa8aa63cf47655f78
Jon Muhovič <jon.muhovic@fri.uni-lj.si>
2026-03-20 11:10:43 +0100

    cleaned up detect_rings and made it more reliable

--- scripts/detect_rings.py
+++ scripts/detect_rings.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import rclpy
 from rclpy.node import Node
-import cv2
+import cv2, math
 import numpy as np
 import tf2_ros
 
@@ -25,26 +25,20 @@ class RingDetector(Node):
         super().__init__('transform_point')
 
         # Basic ROS stuff
-        timer_frequency = 2
+        timer_frequency = 5
         timer_period = 1/timer_frequency
+
+        # ellipse thresholds
+        self.ecc_thr = 100
+        self.ratio_thr = 1.5
+        self.center_thr = 10
 
         # An object we use for converting images between ROS format and OpenCV format
         self.bridge = CvBridge()
 
-        # Marker array object used for visualizations
-        self.marker_array = MarkerArray()
-        self.marker_num = 1
-
         # Subscribe to the image and/or depth topic
         self.image_sub = self.create_subscription(Image, "/oakd/rgb/preview/image_raw", self.image_callback, 1)
         self.depth_sub = self.create_subscription(Image, "/oakd/rgb/preview/depth", self.depth_callback, 1)
-
-        # Publiser for the visualization markers
-        # self.marker_pub = self.create_publisher(Marker, "/ring", QoSReliabilityPolicy.BEST_EFFORT)
-
-        # Object we use for transforming between coordinate frames
-        # self.tf_buf = tf2_ros.Buffer()
-        # self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
 
         cv2.namedWindow("Binary Image", cv2.WINDOW_NORMAL)
         cv2.namedWindow("Detected contours", cv2.WINDOW_NORMAL)
@@ -52,55 +46,50 @@ class RingDetector(Node):
         cv2.namedWindow("Depth window", cv2.WINDOW_NORMAL)        
 
     def image_callback(self, data):
-        self.get_logger().info(f"I got a new image! Will try to find rings...")
-
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        blue = cv_image[:,:,0]
-        green = cv_image[:,:,1]
-        red = cv_image[:,:,2]
-
         # Tranform image to grayscale
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        # gray = red
 
-        # Apply Gaussian Blur
-        # gray = cv2.GaussianBlur(gray,(3,3),0)
-
-        # Do histogram equalization
-        # gray = cv2.equalizeHist(gray)
-
-        # Binarize the image, there are different ways to do it
-        #ret, thresh = cv2.threshold(img, 50, 255, 0)
-        #ret, thresh = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
+        # Binarize the image
         thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 30)
         cv2.imshow("Binary Image", thresh)
-        cv2.waitKey(1)
 
         # Extract contours
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         # Example of how to draw the contours, only for visualization purposes
-        cv2.drawContours(gray, contours, -1, (255, 0, 0), 3)
+        cv2.drawContours(gray, contours, -1, (255, 0, 0), 1)
         cv2.imshow("Detected contours", gray)
-        cv2.waitKey(1)
 
         # Fit elipses to all extracted contours
         elps = []
         for cnt in contours:
-            #     print cnt
-            #     print cnt.shape
             if cnt.shape[0] >= 20:
                 ellipse = cv2.fitEllipse(cnt)
-                elps.append(ellipse)
 
+                # filter ellipses that are too eccentric
+                e = ellipse[1]
+                ecc1 = e[0]
+                ecc2 = e[1]
+
+                ratio = ecc1/ecc2 if ecc1>ecc2 else ecc2/ecc1
+                if ratio<=self.ratio_thr and ecc1<self.ecc_thr and ecc2<self.ecc_thr:
+
+                    elps.append(ellipse)
 
         # Find two elipses with same centers
         candidates = []
         for n in range(len(elps)):
+            e1 = elps[n]
+
+            # display candidates
+            cv2.ellipse(cv_image, e1, (255, 255, 0), 1)
+            cv2.circle(cv_image, (int(e1[0][0]), int(e1[0][1])), 1, (255, 255, 0), -1)
+
             for m in range(n + 1, len(elps)):
                 # e[0] is the center of the ellipse (x,y), e[1] are the lengths of major and minor axis (major, minor), e[2] is the rotation in degrees
                 
@@ -109,12 +98,9 @@ class RingDetector(Node):
                 dist = np.sqrt(((e1[0][0] - e2[0][0]) ** 2 + (e1[0][1] - e2[0][1]) ** 2))
                 angle_diff = np.abs(e1[2] - e2[2])
 
-                # The centers of the two elipses should be within 5 pixels of each other (is there a better treshold?)
-                if dist >= 5:
-                    continue
+                # The centers of the two elipses should be within 10 pixels of each other
 
-                # The rotation of the elipses should be whitin 4 degrees of eachother
-                if angle_diff>4:
+                if dist >= self.center_thr:
                     continue
 
                 e1_minor_axis = e1[1][0]
@@ -131,14 +117,6 @@ class RingDetector(Node):
                     se = e1 # e1 is smaller ellipse
                 else:
                     continue # if one ellipse does not contain the other, it is not a ring
-                
-                # # The widths of the ring along the major and minor axis should be roughly the same
-                # border_major = (le[1][1]-se[1][1])/2
-                # border_minor = (le[1][0]-se[1][0])/2
-                # border_diff = np.abs(border_major - border_minor)
-
-                # if border_diff>4:
-                #     continue
                     
                 candidates.append((e1,e2))
 
@@ -169,8 +147,7 @@ class RingDetector(Node):
             y_min = y1 if y1 > 0 else 0
             y_max = y2 if y2 < cv_image.shape[1] else cv_image.shape[1]
 
-        # if len(candidates)>0:
-        cv2.imshow("Detected rings",cv_image)
+        cv2.imshow("Detected rings", cv_image)
         cv2.waitKey(1)
 
     def depth_callback(self,data):
